1676df4d9f5c3ffb1994bbd13d66a7dfaae63b4e
vipin <107750@dypatiluniversitypune.edu.in>
2022-10-31 12:49:28 +0530

    create AsyncConsumer

--- Dj_CWS1/app/consumers.py
+++ Dj_CWS1/app/consumers.py
@@ -1,4 +1,4 @@
-from channels.consumer import SyncConsumer
+from channels.consumer import SyncConsumer, AsyncConsumer
 
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self,event):
@@ -15,6 +15,28 @@ class MySyncConsumer(SyncConsumer):
         print("Message Recived.....")
 
     def websocket_disconnect(self, event):
+        """
+        This handler is called when either connction to the clinet is lost,
+        either from client closing the connection, the server closing the
+        connection, or loss of socket.
+        """
+        print("WebSocket Disconnected.....")
+
+class MyAyncConsumer(AsyncConsumer):
+    async def websocket_connect(self,event):
+        """
+        This handler is called when clinet initially opens a
+        connection and is about to finish the WebSocket handshake
+        """
+        print("WebSocket Connected....")
+
+    async def websocket_receive(self, event):
+        """
+        This handler is called when data recived from client
+        """
+        print("Message Recived.....")
+
+    async def websocket_disconnect(self, event):
         """
         This handler is called when either connction to the clinet is lost,
         either from client closing the connection, the server closing the
